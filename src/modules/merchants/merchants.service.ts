6e13b4f72e52738dd0f28f8c97689c7002417ee9
Samad191 <samad13354@gmail.com>
2025-09-23 00:32:02 +0500

    feat: added try catch in merchant create service

--- src/modules/merchants/merchants.service.ts
+++ src/modules/merchants/merchants.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateMerchantDto } from './dto/create-merchant.dto';
 import { UpdateMerchantDto } from './dto/update-merchant.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Merchant } from 'src/database/tables/merchant.entity';
 import { Repository } from 'typeorm';
+import { response } from 'express';
 
 @Injectable()
 export class MerchantsService {
@@ -12,9 +13,26 @@ export class MerchantsService {
     private readonly merchantRepository: Repository<Merchant>,
   ) {}
 
-  async create(merchantData: CreateMerchantDto): Promise<CreateMerchantDto> {
-    const merchant = this.merchantRepository.create(merchantData);
-    return await this.merchantRepository.save(merchant);
+  async create(merchantData: CreateMerchantDto) {
+    try {
+      const merchant = this.merchantRepository.create(merchantData);
+      const savedMerchant = await this.merchantRepository.save(merchant);
+      return {
+        statusCode: 201,
+        success: true,
+        message: 'Merchant created successfully',
+        data: savedMerchant,
+      };
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_GATEWAY,
+          error: 'Failed to create merchant',
+          message: error?.response?.data?.error || error.message,
+        },
+        HttpStatus.BAD_GATEWAY,
+      );
+    }
   }
 
   async findByBusinessName(businessName: string): Promise<Merchant | null> {
